a62c49228ae9633942108b6ba81e0607d6cea24f
Steven Seifried <gitlab@canox.net>
2024-12-30 13:59:32 +0100

    Modified for TUXEDO devices

--- README.md
+++ README.md
@@ -1,44 +1,17 @@
-# fnlock-switch-tp-comp-usb-kb
-GNOME Shell Extension for Lenovo ThinkPad Compact USB Keyboard that adds switch FnLock button to GNOME top panel
-https://extensions.gnome.org/extension/3939/fnlock-switch-thinkpad-compact-usb-keyboard/
+# TUXEDO FnLock state
+GNOME Shell Extension to display the FnLock state of TUXEDO devices
 
-\
-FnLock is active (locked)\
-![screenshot01](https://github.com/goloshubov/tp-comp-keyboard-fnlk-switch/blob/master/about/screenshots/locked.png)
+**Hint:** I'm a TUXEDO staff member, but this project is not affiliated to TUXEDO and/or not sponsored by TUXEDO. \
+It was created at my own intention.
 
-FnLock is inactive (unlocked)\
-![screenshot00](https://github.com/goloshubov/tp-comp-keyboard-fnlk-switch/blob/master/about/screenshots/unlocked.png)
+FnLock is enabled (locked)\
+![FnLock on](./about/screenshots/enabled.png)
+
+FnLock is disabled\
+![FnLock off](./about/screenshots/disabled.png)
 
 FnLock not found\
-![screenshot01](https://github.com/goloshubov/tp-comp-keyboard-fnlk-switch/blob/master/about/screenshots/none.png)
+![FnLock not found](./about/screenshots/none.png)
 
-Keybinding by default:\
-\<Control> + Escape
 
-Custom keybinding workaround (TBD: prefs)
-```bash
-cat << EOF | dconf load /org/gnome/shell/extensions/fnlock/
-[/]
-keybinding=['<Control>Escape']
-EOF
-```
-
-## Why?
-There is an issue with Lenovo ThinkPad Compact USB Keyboard - FnLk doesn't work by pressing Fn+Esc in Linux. Manual toggling by writing 1|0 into `/sys/bus/hid/devices/*17EF\:604*/fn_loc` is possible though, thanks to: https://github.com/lentinj/tp-compact-keyboard.
-
-## Configuration
-Notice that (GNOME) user should have write access to `/sys/bus/hid/devices/*17EF\:604*/fn_loc`
-
-```
-cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules
-SUBSYSTEM=="hid", ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="604*", \
-    RUN += "/bin/sh -c 'chown change_to_your_username \"/sys/$devpath/fn_lock\"'"
-EOF
-```
-or
-```
-cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules
-SUBSYSTEM=="hid", ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="604*", \
-    RUN += "/bin/sh -c 'chmod 0660 \"/sys/$devpath/fn_lock\"'"
-EOF
-```
+Forked from and based on [goloshubov/fnlock-switch-tp-comp-usb-kb](https://github.com/goloshubov/fnlock-switch-tp-comp-usb-kb)
---
+++ tuxedo-fnlock-state@stevenseifried.github.io/extension.js
@@ -0,0 +1,140 @@
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import St from "gi://St";
+import Gio from "gi://Gio";
+import GLib from "gi://GLib";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import Meta from "gi://Meta";
+import Shell from "gi://Shell";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as ExtensionUtils from "resource:///org/gnome/shell/misc/extensionUtils.js";
+
+const FNLOCKED_ICON = "fnlk.svg";
+const FNUNLOCKED_ICON = "fn.svg";
+const NOFNLOCK_ICON = "none.svg";
+const FN_LOCK_PATH = "/sys/devices/platform/tuxedo_keyboard/fn_lock";
+
+export default class FnLockExtension extends Extension {
+  constructor(metadata) {
+    super(metadata);
+    this._fileMonitor = null;
+    this._timeout = null;
+  }
+
+  _readFnLockState() {
+    try {
+      return GLib.file_get_contents(FN_LOCK_PATH)[1].toString().trim();
+    } catch (e) {
+      logError(`Error reading fn_lock state: ${e}`);
+      return null;
+    }
+  }
+
+  _writeFnLockState(state) {
+    try {
+      GLib.file_set_contents(FN_LOCK_PATH, state);
+      return true;
+    } catch (e) {
+      logError(`Error writing fn_lock state: ${e}`);
+      return false;
+    }
+  }
+
+  update_fnlock() {
+    const state = this._readFnLockState();
+    
+    if (state === "1") {
+      this._icon.set_gicon(this._gicon_locked);
+    } else if (state === "0") {
+      this._icon.set_gicon(this._gicon_unlocked);
+    } else {
+      this._icon.set_gicon(this._gicon_none);
+    }
+    
+    // Log the current state for debugging
+    log(`Current fn_lock state: ${state}`);
+  }
+
+  switch_fnlock() {
+    const currentState = this._readFnLockState();
+    if (currentState !== null) {
+      const newState = currentState === "1" ? "0" : "1";
+      if (this._writeFnLockState(newState)) {
+        log(`Switched fn_lock state to: ${newState}`);
+        // Add a small delay before updating the icon
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+          this.update_fnlock();
+          return GLib.SOURCE_REMOVE;
+        });
+      }
+    }
+  }
+
+  enable() {
+    this._button = new PanelMenu.Button(0.0);
+    this._icon = new St.Icon({ style_class: "system-status-icon" });
+    this._gicon_locked = Gio.icon_new_for_string(
+      `${this.path}/icons/${FNLOCKED_ICON}`
+    );
+    this._gicon_unlocked = Gio.icon_new_for_string(
+      `${this.path}/icons/${FNUNLOCKED_ICON}`
+    );
+    this._gicon_none = Gio.icon_new_for_string(
+      `${this.path}/icons/${NOFNLOCK_ICON}`
+    );
+
+    this.update_fnlock();
+
+    this._button.add_child(this._icon);
+    this._button.connect("button-press-event", () => {
+      this.switch_fnlock();
+      return Clutter.EVENT_STOP;
+    });
+
+    Main.panel.addToStatusArea("FnLock", this._button);
+
+    this._settings = this.getSettings();
+    Main.wm.addKeybinding(
+      "keybinding",
+      this._settings,
+      Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
+      Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+      () => this.switch_fnlock()
+    );
+
+    // Set up file monitor
+    const file = Gio.File.new_for_path(FN_LOCK_PATH);
+    this._fileMonitor = file.monitor_file(Gio.FileMonitorFlags.NONE, null);
+    this._fileMonitor.connect("changed", () => {
+      log("File monitor detected change");
+      this.update_fnlock();
+    });
+
+    // Set up periodic checking
+    this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+      this.update_fnlock();
+      return GLib.SOURCE_CONTINUE;
+    });
+  }
+
+  disable() {
+    Main.wm.removeKeybinding("keybinding");
+    this._button?.destroy();
+    this._button = null;
+    this._icon?.destroy();
+    this._icon = null;
+    this._gicon_locked = null;
+    this._gicon_unlocked = null;
+    this._gicon_none = null;
+    this._settings = null;
+
+    if (this._fileMonitor) {
+      this._fileMonitor.cancel();
+      this._fileMonitor = null;
+    }
+
+    if (this._timeout) {
+      GLib.source_remove(this._timeout);
+      this._timeout = null;
+    }
+  }
+}
